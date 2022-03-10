032132fa23c733dceb24b17735e22f94eb4a176f
root <tubonemidaniel@gmail.com>
2022-03-10 15:53:18 -0800

    sdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@ int main(void)
 	/*use puts()*/
 
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 
 	return (0);
 }
