457a5e50473a03a462450a653eccf24e679462c2
root <tubonemidaniel@gmail.com>
2022-03-10 15:56:07 -0800

    afd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-
 /*use puts function */
 int main(void)
-/*here's the main function*/
-
 {
-
 	/*use puts()*/
-
-
 	puts("\"Programming is like building a multilingual puzzle\n");
-
-	return(0);
+	return (0);
 }
