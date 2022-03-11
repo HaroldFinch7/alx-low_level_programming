91cf4850c6085c297e525f05af95621b90394285
root <tubonemidaniel@gmail.com>
2022-03-10 16:02:35 -0800

    sdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void) /*description*/
+int main(void) /*description: this is the how the code runs*/
 {
 	/*use puts()*/
 	puts("\"Programming is like building a multilingual puzzle");
