2b74b6bc1d987452a38770b101479e2cbabeef65
root <tubonemidaniel@gmail.com>
2022-03-12 05:07:41 -0800

    asfv

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,11 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c != 'e' || c != 'q')
-			putchar(c);
+		if (c != 'e')
+		{
+			if (c != 'q')
+				putchar(c);
+		}
 	}
 	putchar('\n');
 	return (0);
