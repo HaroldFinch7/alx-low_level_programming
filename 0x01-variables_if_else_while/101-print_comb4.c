f5316a8441a61c6f30ae1af2e7cbfc353352d4fb
root <tubonemidaniel@gmail.com>
2022-03-14 09:43:35 -0700

    combination of 3 digits- unrepeated

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int o;
+
+	for (n = 0; n < 8; n++)
+	{
+		for  (m = n + 1; m < 9; m++)
+		{
+			for (o = m + 1; o < 10; o++)
+			{
+				putchar((n % 10) + '0');
+				putchar((m % 10) + '0');
+				putchar((m % 10) + '0');
+
+				if (n == 7 && m == 8 && o == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
