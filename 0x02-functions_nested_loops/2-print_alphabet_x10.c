4f4f664bd918f7ea7369c08ea2ff96153db602ae
root <tubonemidaniel@gmail.com>
2022-03-15 12:17:23 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints letters of the alphabet 10 times t
+ * hrough a for loop,
+ * followed a line
+ *
+ * Return: Always void.
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+	}
+
+	_putchar('\n');
+}
