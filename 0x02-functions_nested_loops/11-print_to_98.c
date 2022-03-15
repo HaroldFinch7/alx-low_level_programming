cfd1d660df81af3ae5f30f903317b34fbad19249
root <tubonemidaniel@gmail.com>
2022-03-15 15:21:08 -0700

    stop at 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 -prints to 98 separated by comma and space
+ * @n: the number to start with
+ */
+void print_to_98(int n_)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			print("%d, ", n++);
+		print("%d\n", n);
+	}
+}
