a6b9ee49a251b5d8232987f46f4027d649ec470d
root <tubonemidaniel@gmail.com>
2022-03-15 08:48:21 -0700

    putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+
+
+/**
+ * main - Prints "_putchar",
+ * followed by a new line
+ *
+ * Return: Always 0.
+ */
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
