a27bffd9d83c7876ba261233c5b3d47d0ec8214d
root <tubonemidaniel@gmail.com>
2022-03-16 15:14:05 -0700

    chanemade

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-	int i;
+/*	int i;*/
 
 	printf("Infinte loop incoming :(\n");
 
-	i = 0;
+/*	i = 0;
 
-/*	while (i < 10)
+	while (i < 10)
 	{
 		putchar(i);
 	}
 
-*/	print("infinite loop avoided! \\o/\n");
+*/	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
 }
