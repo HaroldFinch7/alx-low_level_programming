b2e9e55be3821ca4a1959cf73c6d0e46a48953e5
root <tubonemidaniel@gmail.com>
2022-03-17 02:06:38 -0700

    changesmade

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,10 +25,10 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if(month == 2 && day == 60)
-		{
-			printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
+		/*if(month == 2 && day == 60)*/
+		/*{*/
+		/*	printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);*/
+	/*	}*/
 		else
 		{
 			printf("Day of the year: %d\n", day);
