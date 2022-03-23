27f5815839d64c7410ee0846a9f2af081ecb9bbf
root <tubonemidaniel@gmail.com>
2022-03-23 12:05:09 -0700

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * reverse_array - Reverses the content of an array of integers.
+ * reverse_array - Reverses the content
+ * of an array of integers.
  * @a: The array of integers to be reversed.
  * @n: The number of elements in the array.
  */
