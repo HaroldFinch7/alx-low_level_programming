fb63af63f4a308ed3c47363392ef628e018820a6
root <tubonemidaniel@gmail.com>
2022-04-01 08:20:03 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to main
+ * @argv: an array of arguments passed to main
+ * Return: Always 0
+ */
+
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
