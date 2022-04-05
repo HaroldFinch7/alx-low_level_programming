1cee078af05737306de179bdc9c3ea077fbdb4e9
root <tubonemidaniel@gmail.com>
2022-04-05 10:36:07 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of chars and 
+ * create_array - Creates an array of chars and
  * initializes it with a specific char
  * @size: the size of the array
  * @c: the char to initialize the array with
